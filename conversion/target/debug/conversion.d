eed60bc05e19ac85ce4fe9860436a97bfe613917
bsed <me@gnux.cn>
2019-06-20 20:42:02 +0800

    conversion

---
+++ conversion/target/debug/conversion.d
@@ -0,0 +1,1 @@
+/home/kelvin/workspace/rust/rust_example/conversion/target/debug/conversion: /home/kelvin/workspace/rust/rust_example/conversion/src/main.rs
---
+++ conversion/target/debug/deps/conversion-f5424c782405a82d.d
@@ -0,0 +1,5 @@
+/home/kelvin/workspace/rust/rust_example/conversion/target/debug/deps/conversion-f5424c782405a82d: src/main.rs
+
+/home/kelvin/workspace/rust/rust_example/conversion/target/debug/deps/conversion-f5424c782405a82d.d: src/main.rs
+
+src/main.rs:
