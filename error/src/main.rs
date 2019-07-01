904c70023a01e03dd2ab1817d2d2525103dee274
kelvin <me@gnux.cn>
2019-07-01 15:24:38 +0800

    iter_result result

--- error/src/main.rs
+++ error/src/main.rs
@@ -86,6 +86,9 @@ fn main() {
         .into_iter()
         .map(|s| s.parse::<i32>())
         .partition(Result::is_ok);
+
+    let numbers: Vec<_> = numbers.into_iter().map(Result::unwrap).collect();
+    let errors: Vec<_> = errors.into_iter().map(Result::unwrap_err).collect();
     println!("Numbers: {:?}", numbers);
     println!("Errors: {:?}", errors);
 }
