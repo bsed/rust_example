c9ddaaafa33147ab918baa32bd0b5f9f4d02e33f
kelvin <me@gnux.cn>
2019-07-01 14:58:42 +0800

    optin_unwrap

--- error/src/main.rs
+++ error/src/main.rs
@@ -11,6 +11,21 @@ fn give_princess(gift: &str) {
     println!("I love {}s!!!!!", gift);
 }
 
+fn give_commoner(gift: Option<&str>){
+    match gift {
+        Some("snake") => println!("Yuck! I'm throwing that snake in a fire."),
+        Some(inner) => println!("{}? How nice.", inner),
+        None => println!("No gift? Oh well."),
+    }
+}
+
+fn give_princess2(gift: Option<&str>){
+    let inside = gift.unwrap();
+    if inside == "snake" { panic!("AAAaaaaa!!!!"); }
+    println!("I love {}s!!!!!", inside);
+
+}
+
 fn main() {
     println!("Hello, world!");
     let twenty = multiply("10", "2");
@@ -21,4 +36,18 @@ fn main() {
 
     give_princess("teddy bear");
     give_princess("snake");
+
+    let food = Some("chicken");
+    let snake = Some("snake");
+    let void = None;
+
+    give_commoner(food);
+    give_commoner(snake);
+    give_commoner(void);
+
+     let bird = Some("robin");
+    let nothing = None;
+
+    give_princess2(bird);
+    give_princess2(nothing);
 }
