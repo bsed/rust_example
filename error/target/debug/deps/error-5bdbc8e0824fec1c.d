74b34048f502b42febf54cd856421ae301e82d96
kelvin <me@gnux.cn>
2019-07-01 14:35:33 +0800

    'result'

---
+++ error/target/debug/deps/error-5bdbc8e0824fec1c.d
@@ -0,0 +1,5 @@
+C:\workspace\rust\rust_example\error\target\debug\deps\error-5bdbc8e0824fec1c.exe: src\main.rs
+
+C:\workspace\rust\rust_example\error\target\debug\deps\error-5bdbc8e0824fec1c.d: src\main.rs
+
+src\main.rs:
---
+++ error/target/debug/error.d
@@ -0,0 +1,1 @@
+C:\workspace\rust\rust_example\error\target\debug\error.exe: C:\workspace\rust\rust_example\error\src\main.rs
