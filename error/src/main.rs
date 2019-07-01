1bfaa106638f72c31052d6d33e6b598102d5423a
kelvin <me@gnux.cn>
2019-07-01 14:48:22 +0800

    'panic'

--- error/src/main.rs
+++ error/src/main.rs
@@ -4,11 +4,21 @@ fn multiply(first_number_str: &str, second_number_str: &str) -> i32 {
     let second_number = second_number_str.parse::<i32>().unwrap();
     first_number * second_number
 }
+
+fn give_princess(gift: &str) {
+    if gift == "snake" { panic!("AAAaaaaa!!!!"); }
+
+    println!("I love {}s!!!!!", gift);
+}
+
 fn main() {
     println!("Hello, world!");
     let twenty = multiply("10", "2");
     println!("double is {}", twenty);
 
-    let tt = multiply("t", "2");
-    println!("double is {}", tt);
+    // let tt = multiply("t", "2");
+    // println!("double is {}", tt);
+
+    give_princess("teddy bear");
+    give_princess("snake");
 }
