8880b2afc15bf92412565ddf949456703b4f4740
kelvin <me@gnux.cn>
2019-07-02 17:04:58 +0800

    flow_control  DESTRUCTURE_ENUM

--- flow_control/src/main.rs
+++ flow_control/src/main.rs
@@ -7,6 +7,19 @@ enum Foo {
 fn age() -> i32 {
     15
 }
+
+#[allow(dead_code)]
+enum Color {
+    Red,
+    Blue,
+    Green,
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+    CMYK(u32, u32, u32, u32),
+}
+
 fn main() {
     println!("Hello, world!");
 
@@ -224,4 +237,23 @@ fn main() {
         (x, _) if x % 2 == 1 => println!("The first one is odd"),
         _ => println!("No correlation..."),
     }
+
+    let color11 = Color::RGB(122 , 17, 40);
+     println!("What color is it?");
+     match color11 {
+        Color::Red   => println!("The color is Red!"),
+        Color::Blue  => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) =>
+            println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) =>
+            println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) =>
+            println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
+                c, m, y, k),
+     }
 }
