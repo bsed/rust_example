70c3798313f21f5dc740f325da03f6dae71a4c8b
kelvin <me@gnux.cn>
2019-07-02 16:22:24 +0800

    flow_control loop

--- flow_control/src/main.rs
+++ flow_control/src/main.rs
@@ -100,4 +100,21 @@ fn main() {
         println!("c is {}", value);
     }
 
+    let mut count = 0u32;
+    println!("Let's count unitl infinity!");
+
+    loop {
+        count += 1;
+        if count == 3 {
+            println!("three");
+            // 跳过这次迭代的剩下内容
+            continue;
+        }
+        println!("{}", count);
+        if count == 5 {
+            println!("OK, that's enough");
+            // 退出循环
+            break;
+        }
+    }
 }
