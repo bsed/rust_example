7e3752caa15a8db32a1ec2c6ddd8b8e5e863fd33
kelvin <me@gnux.cn>
2019-07-02 16:36:51 +0800

    flow_control while

--- flow_control/src/main.rs
+++ flow_control/src/main.rs
@@ -136,4 +136,19 @@ fn main() {
     };
 
     println!("{} -> {}", boolean4, binary4);
+
+    let mut n5 = 1;
+    while n5 < 101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        }else if n % 3 == 0 {
+            println!("fizz");
+        }else if n % 5 == 0 {
+            println!("buzz");
+        }else {
+            println!("{}", n);
+        }
+
+        n5 += 1;
+    }
 }
