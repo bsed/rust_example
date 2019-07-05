8c0d241b9d495ebb2c1010f0cb8722ae8455754c
kelvin <me@gnux.cn>
2019-07-05 14:41:04 +0800

    hello

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,41 @@
+use std::fmt::{self, Formatter, Display};
+
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32,
+}
+
+
+impl Display for City {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        write!(f, "{}: {:.3}°{} {:.3}°{}",
+               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+#[derive(Debug)]
+struct Structure(i32);
+
+#[derive(Debug)]
+struct Deep(Structure);
+
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8
+}
+
 fn main() {
     println!("{} days.", 31);
     println!("{0}, this {1}. {1}, this is {0}", "Alice","Bob");
@@ -7,6 +45,7 @@ fn main() {
         subject="the quick brown fox",
         verb="jump over");
     
+    
     println!("{} of {:b} people kknow binary, the other half don't", 1, 2);
 
     println!("{number:>0width$}", number=1, width=6);
@@ -14,4 +53,34 @@ fn main() {
 
     println!("My name is {0}, {1}, {0}", "Bound", "Lee");
 
+
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+
+       for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // 在添加了针对 fmt::Display 的实现后，请改用 {} 检验效果。
+        println!("{:?}", *color)
+    }
+
+    println!("{:?} months in a year. ", 12);
+    println!("{1:?} {0:?} is the {actor:?} name.",
+             "Slater",
+             "Christian",
+             actor="actor's");
+    println!("Now {:?} will print!", Structure(3));
+    println!("Now {:?} will print!", Deep(Structure(7)));
+
+    let name = "Peter";
+    let age = 27;
+    let peter = Person {name, age};
+    println!("{:#?}", peter);
 }
