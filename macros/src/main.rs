1c797c1daeba9f3933e8c296ae5604544d407fcb
kelvin <me@gnux.cn>
2019-07-05 17:27:31 +0800

    macros repeat

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -42,6 +42,13 @@ macro_rules! test {
                  $left || $right)
     );
 }
+
+macro_rules! find_min {
+    ($x:expr) => ($x);
+    ($x:expr, $($y:expr),+) => {
+        std::cmp::min($x, find_min!($($y),+))
+    }
+}
 fn main() {
     foo();
     bar();
@@ -60,6 +67,10 @@ fn main() {
         eval (1 + 2) * (3 / 4)
     }
 
-     test!(1i32 + 1 == 2i32; and 2i32 * 2 == 4i32);
+    test!(1i32 + 1 == 2i32; and 2i32 * 2 == 4i32);
     test!(true; or false);
+
+    println!("{}", find_min!(1u32));
+    println!("{}", find_min!(1u32 + 2 , 2u32));
+    println!("{}", find_min!(5u32, 2u32 * 3, 4u32));
 }
