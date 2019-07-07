74178ec8d297a98a96243b56e4396e441404c8b9
kelvin <me@gnux.cn>
2019-07-07 16:04:50 +0800

    primitives literals

--- primitives/src/main.rs
+++ primitives/src/main.rs
@@ -22,4 +22,23 @@ fn main() {
     analyze_slice(&ys[1 .. 3]);
 
     println!("{}", xs[4]);
+
+
+    println!("1 + 2 = {}", 1i32 + 2);
+    println!("1 - 2 = {}", 1i32 -2);
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+     // 位运算
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    println!("One million is written as {}", 1_000_000u32);
+
+    
 }
