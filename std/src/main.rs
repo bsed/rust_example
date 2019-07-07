8a33e24130fd66c29c9d64dd20542aa6b23698ee
kelvin <me@gnux.cn>
2019-07-07 17:09:40 +0800

    std hashmap

--- std/src/main.rs
+++ std/src/main.rs
@@ -1,4 +1,6 @@
 use std::mem;
+use std::collections::HashMap;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy)]
 struct Point {
@@ -19,6 +21,17 @@ fn origin() -> Point {
 fn boxed_origin() -> Box<Point> {
     Box::new(Point {x: 0.0, y: 0.0})
 }
+
+fn call(number: &str) -> &str {
+    match number {
+        "798-1364" => "We're sorry, the call cannot be completed as dialed. 
+            Please hang up and try again.",
+        "645-7689" => "Hello, this is Mr. Awesome's Pizza. My name is Fred.
+            What can I get for you today?",
+        _ => "Hi! Who is this again?"
+    }
+}
+
 
 fn main() {
     println!("Hello, world!");
@@ -51,4 +64,27 @@ fn main() {
     let unboxed_point: Point = *boxed_point;
     println!("Unboxed point occupies {} bytes in the stack",
              mem::size_of_val(&unboxed_point));
+
+    let mut contacts = HashMap::new();
+    contacts.insert("Daniel", "798-1364");
+    contacts.insert("Ashley", "645-7689");
+    contacts.insert("Katie", "435-8291");
+    contacts.insert("Robert", "956-1745");
+
+    match contacts.get(&"Daniel"){
+        Some(&number) => println!("Calling Daniel: {}", call(number)),
+        _ => println!("Don't have Daniel's number. "),
+    }
+
+    contacts.remove(&("Ashley"));
+    match contacts.get(&"Ashley") {
+        Some(&number) => println!("Calling Ashley: {}", call(number)),
+        _ => println!("Don't have Ashley's number."),
+    }
+
+    contacts.remove(&("Ashley")); 
+    for (contact, &number) in contacts.iter() {
+         println!("Calling1 {}: {}", contact, call(number)); 
+    }
+
 }
