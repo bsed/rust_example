46caa8c6b645bfb175a5ba23e43ed9ba331191e4
kelvin <me@gnux.cn>
2019-07-08 16:09:42 +0800

    std_misc fi;e

--- std_misc/src/main.rs
+++ std_misc/src/main.rs
@@ -5,6 +5,9 @@ use std::process::Command;
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
+use std::io::prelude::*;
+use std::error::Error;
+use std::fs::File;
 
 #[link(name = "m")]
 extern "C" {
@@ -31,6 +34,15 @@ impl fmt::Debug for Complex {
         }
     }
 }
+
+static LOREM_IPSUM: &'static str = 
+"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+";
 
 static NTHREADS: i32 = 10;
 fn main() {
@@ -97,5 +109,23 @@ fn main() {
 
     for child in children {
         let _ = child.join();
+    }
+
+    let path = Path::new("out/lorem_ipsum.txt");
+    let display = path.display();
+
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}",
+                           display,
+                           why.description()),
+        Ok(file) => file,
+    };
+
+    match file.write_all(LOREM_IPSUM.as_bytes()) {
+        Err(why) => {
+            panic!("couldn't write to {}: {}", display,
+                                               why.description())
+        },
+        Ok(_) => println!("successfully wrote to {}", display),
     }
 }
