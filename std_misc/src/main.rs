5acfc4fe67bc62d5eaab44b562315191593266ff
kelvin <me@gnux.cn>
2019-07-08 15:18:07 +0800

    std_misc channel

--- std_misc/src/main.rs
+++ std_misc/src/main.rs
@@ -1,8 +1,32 @@
 use std::env;
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
+use std::thread;
+
+static NTHREADS: i32 = 3;
+
 
 fn main() {
     println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
     println!("My path is {}.", args[0]);
     println!("I got {:?} arguments: {:?}", args.len()-1, &args[1..]);
+
+    let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+    for id in 0..NTHREADS {
+        let thread_tx = tx.clone();
+        thread::spawn(move || {
+            thread_tx.send(id).unwrap();
+
+            println!("thread {} finished", id);
+        });
+    }
+
+    let mut ids = Vec::with_capacity(NTHREADS as usize);
+    for _ in 0..NTHREADS {
+        ids.push(rx.recv());
+    }
+
+    println!("{:?}", ids);
+
 }
