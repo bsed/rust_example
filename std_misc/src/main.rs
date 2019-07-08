7a14117572656344051f458ac2ff1904ac74e364
kelvin <me@gnux.cn>
2019-07-08 15:23:55 +0800

    std_misc ffi'

--- std_misc/src/main.rs
+++ std_misc/src/main.rs
@@ -2,6 +2,33 @@ use std::env;
 use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
 use std::thread;
+use std::fmt;
+
+#[link(name = "m")]
+extern {
+    fn csqrtf(z: Complex) -> Complex;
+    fn ccosf(z: Complex) -> Complex;
+}
+
+fn cos(z: Complex) -> Complex {
+    unsafe {ccosf(z)}
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct Complex {
+    re: f32,
+    im: f32,
+}
+impl fmt::Debug for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.im < 0. {
+            write!(f, "{}-{}i", self.re, -self.im)
+        } else {
+            write!(f, "{}+{}i", self.re, self.im)
+        }
+    }
+}
 
 static NTHREADS: i32 = 3;
 
@@ -28,5 +55,13 @@ fn main() {
     }
 
     println!("{:?}", ids);
+
+     let z = Complex { re: -1., im: 0. };
+
+    let z_sqrt = unsafe { csqrtf(z) };
+
+    println!("the square root of {:?} is {:?}", z, z_sqrt);
+
+    println!("cos({:?}) = {:?}", z, cos(z));
 
 }
