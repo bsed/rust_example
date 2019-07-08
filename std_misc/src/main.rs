0d1b5904e521400506e3a25e99940ac608111ce0
kelvin <me@gnux.cn>
2019-07-08 16:34:41 +0800

    std_misc wait

--- std_misc/src/main.rs
+++ std_misc/src/main.rs
@@ -1,13 +1,15 @@
 use std::env;
+use std::error::Error;
 use std::fmt;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufRead};
 use std::path::Path;
+//use std::process::{Command, Stdio};
 use std::process::Command;
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
-use std::io::prelude::*;
-use std::error::Error;
-use std::fs::File;
 
 #[link(name = "m")]
 extern "C" {
@@ -35,14 +37,25 @@ impl fmt::Debug for Complex {
     }
 }
 
-static LOREM_IPSUM: &'static str = 
-"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+static LOREM_IPSUM: &'static str =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 ";
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[allow(dead_code)]
+static PANGRAM: &'static str = "the quick brown fox jumped over the lazy dog\n";
 
 static NTHREADS: i32 = 10;
 fn main() {
@@ -115,17 +128,57 @@ fn main() {
     let display = path.display();
 
     let mut file = match File::create(&path) {
-        Err(why) => panic!("couldn't create {}: {}",
-                           display,
-                           why.description()),
+        Err(why) => panic!("couldn't create {}: {}", display, why.description()),
         Ok(file) => file,
     };
 
     match file.write_all(LOREM_IPSUM.as_bytes()) {
-        Err(why) => {
-            panic!("couldn't write to {}: {}", display,
-                                               why.description())
-        },
+        Err(why) => panic!("couldn't write to {}: {}", display, why.description()),
         Ok(_) => println!("successfully wrote to {}", display),
     }
+
+    let path1 = Path::new("hello.txt");
+    let display1 = path1.display();
+    let mut file1 = match File::open(&path1) {
+        Err(why) => panic!("couldn't open {}: {}", display1, why.description()),
+        Ok(file1) => file1,
+    };
+
+    let mut s1 = String::new();
+    match file1.read_to_string(&mut s1) {
+        Err(why) => panic!("couldn't read {}: {} ", display1, why.description()),
+        Ok(_) => print!("{} contains:\n{}", display1, s1),
+    }
+
+    if let Ok(lines) = read_lines("./hosts") {
+        for line in lines {
+            if let Ok(ip) = line {
+                println!("{}", ip);
+            }
+        }
+    }
+
+    // let process2 = match Command::new("wc")
+    //     .stdin(Stdio::piped())
+    //     .stdout(Stdio::piped())
+    //     .spawn()
+    // {
+    //     Err(why) => panic!("couldn't spawn wc: {}", why.description()),
+    //     Ok(process2) => process2,
+    // };
+    // match process2.stdin.unwrap().write_all(PANGRAM.as_bytes()) {
+    //     Err(why) => panic!("couldn't write to wc stdin: {}", why.description()),
+    //     Ok(_) => println!("sent pangram to wc"),
+    // }
+
+    // let mut s = String::new();
+    // match process2.stdout.unwrap().read_to_string(&mut s) {
+    //     Err(why) => panic!("couldn't read wc stdout: {}",
+    //                        why.description()),
+    //     Ok(_) => print!("wc responded with:\n{}", s),
+    // }
+
+    let mut child3 = Command::new("sleep").arg("5").spawn().unwrap();
+    let _result = child3.wait().unwrap();
+    println!("reached end of main");
 }
