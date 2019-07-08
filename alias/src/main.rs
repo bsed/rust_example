2877665e7be2877c7cec8486da16e93c7118f761
kelvin <me@gnux.cn>
2019-07-08 11:22:23 +0800

    types cast

--- alias/src/main.rs
+++ alias/src/main.rs
@@ -3,7 +3,7 @@ type Inch = u64;
 
 #[allow(non_camel_case_types)]
 type u64_t = u64;
-
+#[allow(overflowing_literals)]
 
 fn main() {
     println!("Hello, world!");
@@ -14,4 +14,22 @@ fn main() {
              nanoseconds,
              inches,
              nanoseconds + inches);
+    let decimal = 65.4321_f32;
+    //let integer: u8 = decimal;
+    let integer = decimal as u8;
+    let character = integer as char;
+    println!("Casting: {} -> {} -> {}", decimal, integer, character);
+
+    println!("1000 as a u16 is: {}", 1000 as u16);
+    println!("1000 as a u8 is : {}", 1000 as u8);
+
+    println!("  -1 as a u8 is : {}", (-1i8) as u8);
+
+    println!("1000 mod 256 is : {}", 1000 % 256);
+    println!(" 128 as a i16 is: {}", 128 as i16);
+    println!(" 128 as a i8 is : {}", 128 as i8);
+
+    println!("1000 as a u8 is : {}", 1000 as u8);
+    // 232 的二进制补码是 -24
+    println!(" 232 as a i8 is : {}", 232 as i8);
 }
